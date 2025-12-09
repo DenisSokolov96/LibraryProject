395b14f183719818ea72931dffea23035d8369e0
DenisSokolov96 <dens111296@gmail.com>
2025-12-09 20:43:28 +0300

    feat: Небольшие правки по стилям

--- src/app/common-ui/book-card/book-card.component.ts
+++ src/app/common-ui/book-card/book-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Book } from '../../data/interfaces/book.interface';
 
 @Component({
@@ -7,8 +7,14 @@ import { Book } from '../../data/interfaces/book.interface';
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.scss'
 })
-export class BookCardComponent {
-  @Input() book!: Book;
+export class BookCardComponent implements OnChanges {
+  @Input({ required: true }) book!: Book;
+
+    ngOnChanges(changes: SimpleChanges): void {
+    if (changes['book'] && this.book) {
+      console.log(`Книга загружена или обновлена: ${this.book.title}`);
+    }
+  }
 
   getDownloadFileName(): string {
     return `${this.book.title}_${this.book.author.shortName}_${this.book.resource}`;
